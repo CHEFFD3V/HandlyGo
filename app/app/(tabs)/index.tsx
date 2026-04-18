e8ee4ad5e64c5210243125f155b403c68e841b91
Miguel Rochet <mrochet@cardnet.com.do>
2026-04-18 18:58:47 -0400

    feat: construcción del diccionario global inicial #11

--- app/app/(tabs)/index.tsx
+++ app/app/(tabs)/index.tsx
@@ -1,6 +1,19 @@
 import { View, Text } from "react-native";
+import { useEffect } from 'react';
+import { getDictionaryByCategory } from '../../services/dictionaryService';
 
 export default function Home() {
+
+  /* Lógica de prueba
+  useEffect(() => {
+    const testFetch = async () => {
+      const items = await getDictionaryByCategory("Abecedario");
+      console.log("REVISIÓN BACKEND - Elementos encontrados:", items);
+    };
+
+    testFetch();
+  }, []); */
+
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>Pantalla Principal (Tabs)</Text>
