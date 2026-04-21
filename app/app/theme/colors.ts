1b0741c9c105f1920305f03d8f16944d249eacc5
AngelC Dev <angelcuestarodriguez09@gmail.com>
2026-04-21 09:57:25 -0400

    feat: Creacion de archivos .ts para los temas y el uso de tema global con zustand

---
+++ app/app/theme/colors.ts
@@ -0,0 +1,15 @@
+
+
+export const lightTheme = {
+  background: "#FFFFFF",
+  text: "#000000",
+  primary: "#2200CC",
+  card: "#F9FAFB",
+};
+
+export const darkTheme = {
+  background: "#0F172A",
+  text: "#FFFFFF",
+  primary: "#4F46E5",
+  card: "#1E293B",
+};
